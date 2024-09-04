9c36710d2867b741c991e724dda523744dd7a53a
phumipatsrisu <phumipatsrisu@gmail.com>
2024-09-04 20:46:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>ข้อมูลของนักเรียน</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">ระบบลงทะเบียน</h1>
+        <hr>
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                    <th>รหัสนักเรียน</th>
+                    <th>ชื่อ-นามสกุล</th>
+                    <th>อีเมลล์</th>
+                    <th>หมายเลขโทรศัพท์</th>   
+                    <th>วันที่ลงทะเบียน</th>   
+                    <th>แก้ไข</th>   
+                    <th>ลบ</th>   
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td><a class="btn btn-primary" href="">แก้ไข</a></td>
+                    <td><a class="btn btn-danger" href="">ลบ</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
